82eb36c5438f04fd2b39cf02249d02926ddb0ce4
Mamadou Mbaye <mmbaye1@hotmail.fr>
2021-08-17 13:26:48 +0200

    ess

---
+++ frontend/app.js
@@ -0,0 +1,20 @@
+getArticles()
+
+function getArticles() {
+    fetch("http://localhost:3000/api/cameras")
+        .then(response => response.json())
+        .then(datas => datas.forEach(article => {
+
+                const elmt = document.getElementById("temp")
+                const clone = document.importNode(elmt.content, true)
+                clone.getElementById("image").src = article.imageUrl
+                clone.getElementById("nomproduit").textContent = article.name
+                clone.getElementById("prix").textContent = article.price / (100) + "€"
+                clone.getElementById("descrip").textContent = article.description
+                clone.getElementById("bloca").href += article._id
+                document.getElementById("sectionproduit").appendChild(clone)
+            })
+
+        )
+        .catch(error => alert(err));
+}
